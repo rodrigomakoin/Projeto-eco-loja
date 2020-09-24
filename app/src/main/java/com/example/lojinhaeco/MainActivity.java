95493289f641b24504c1012eb20e032815d23344
rodrigomakoin <48112596+rodrigomakoin@users.noreply.github.com>
2020-09-24 20:44:52 -0300

    start commit!

---
+++ app/src/main/java/com/example/lojinhaeco/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.lojinhaeco;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.example.lojinhaeco.ui.main.SectionsPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+
+                    }
+        };
